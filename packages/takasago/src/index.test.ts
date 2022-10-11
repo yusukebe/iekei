8279a66185397b9eda74f915ed5a361e1efa358f
Yusuke Wada <yusuke@kamawada.com>
2022-10-11 21:21:14 +0900

    add takasagoya

---
+++ packages/takasago/src/index.test.ts
@@ -0,0 +1,7 @@
+import { takasago } from './index'
+
+describe('takasago', () => {
+  it('Should return たかさご家', () => {
+    expect(takasago()).toBe('たかさご家')
+  })
+})
---
+++ packages/takasago/src/index.ts
@@ -0,0 +1,3 @@
+export const takasago = () => {
+  return 'たかさご家'
+}
