deafc8364c9f0aa0eca69fe22963f5d5807dbcad
Yusuke Wada <yusuke@kamawada.com>
2022-10-13 19:56:12 +0900

    feat(yoshimura): support gu

--- packages/yoshimura/src/index.test.ts
+++ packages/yoshimura/src/index.test.ts
@@ -2,6 +2,6 @@ import { yoshimura } from './index'
 
 describe('yoshimura', () => {
   it('Should return 吉村家', () => {
-    expect(yoshimura()).toEqual({ name: '吉村家' })
+    expect(yoshimura()).toEqual({ name: '吉村家', gu: ['ほうれん草', 'チャーシュー'] })
   })
 })
--- packages/yoshimura/src/index.ts
+++ packages/yoshimura/src/index.ts
@@ -1,5 +1,6 @@
 export const yoshimura = () => {
   return {
     name: '吉村家',
+    gu: ['ほうれん草', 'チャーシュー'],
   }
 }
